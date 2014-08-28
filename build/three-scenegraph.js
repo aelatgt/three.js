0e8338ca3fcf03ba2cf2dd28875c241654172e92
Gheric Speiginer <gheric.speiginer@gmail.com>
2014-08-28 17:06:23 -0400

    Added Light.js back into scenegraph build
    (since Scene.js expects THREE.Light to be defined)

--- build/three-scenegraph.js
+++ build/three-scenegraph.js
@@ -8872,6 +8872,35 @@ THREE.PerspectiveCamera.prototype.clone = function () {
 	return camera;
 };
 
+// File:src/lights/Light.js
+
+/**
+ * @author mrdoob / http://mrdoob.com/
+ * @author alteredq / http://alteredqualia.com/
+ */
+
+THREE.Light = function ( color ) {
+
+	THREE.Object3D.call( this );
+
+	this.color = new THREE.Color( color );
+
+};
+
+THREE.Light.prototype = Object.create( THREE.Object3D.prototype );
+
+THREE.Light.prototype.clone = function ( light ) {
+
+	if ( light === undefined ) light = new THREE.Light();
+
+	THREE.Object3D.prototype.clone.call( this, light );
+
+	light.color.copy( this.color );
+
+	return light;
+
+};
+
 // File:src/scenes/Scene.js
 
 /**
