f3779834e6533d8b4e4d2c99917c8bec36ee726e
Gheric Speiginer <gheric.speiginer@gmail.com>
2014-08-28 21:25:16 -0400

    Added Bone.js back into scenegraph build
    (since Scene.js expects THREE.Bone to be defined)

--- build/three-scenegraph.js
+++ build/three-scenegraph.js
@@ -8901,6 +8901,41 @@ THREE.Light.prototype.clone = function ( light ) {
 
 };
 
+// File:src/objects/Bone.js
+
+/**
+ * @author mikael emtinger / http://gomo.se/
+ * @author alteredq / http://alteredqualia.com/
+ * @author ikerr / http://verold.com
+ */
+
+THREE.Bone = function ( belongsToSkin ) {
+
+	THREE.Object3D.call( this );
+
+	this.skin = belongsToSkin;
+
+	this.accumulatedRotWeight = 0;
+	this.accumulatedPosWeight = 0;
+	this.accumulatedSclWeight = 0;
+
+};
+
+THREE.Bone.prototype = Object.create( THREE.Object3D.prototype );
+
+THREE.Bone.prototype.updateMatrixWorld = function ( force ) {
+
+	THREE.Object3D.prototype.updateMatrixWorld.call( this, force );
+
+	// Reset weights to be re-accumulated in the next frame
+
+	this.accumulatedRotWeight = 0;
+	this.accumulatedPosWeight = 0;
+	this.accumulatedSclWeight = 0;
+
+};
+
+
 // File:src/scenes/Scene.js
 
 /**
